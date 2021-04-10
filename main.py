f407b7cc899d79f7ede6015007c7166e9accf8ad
ArtemBiliksin <ArtemBiliksin@users.noreply.github.com>
2021-04-10 20:11:44 +0500

    Лабораторная работа 5. Обновление

--- main.py
+++ main.py
@@ -1,80 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from matplotlib.widgets import TextBox, CheckButtons
 from method import Method
 
 
 def calculate_x_data_y_data(n):
-    m = Method(n)
+
+    method = Method(n)
     x_data = np.linspace(0, 1, n)
-    y_data_method_euler = [0.1]
-    y_data_method_cauchy = [0.1]
-    y_data_method_trapezium = [0.1]
-
+    dict_methods = method.dict_methods
+    y_data_dict = dict(zip(dict_methods.keys(), [[0.1] for _ in range(3)]))
     for x in x_data[1:]:
-        y_data_method_euler.append(m.method_euler(x, y_data_method_euler[-1]))
-        y_data_method_cauchy.append(m.method_cauchy(x, y_data_method_cauchy[-1]))
-        y_data_method_trapezium.append(m.method_trapezium(x, y_data_method_trapezium[-1]))
-    return x_data, y_data_method_euler, y_data_method_cauchy, y_data_method_trapezium
+        for method_name in dict_methods:
+            y_data_dict[method_name].append(dict_methods[method_name](x, y_data_dict[method_name][-1]))
+
+    return x_data, y_data_dict
 
 
-def add_solution_plot_to_axes():
-
-    def y_data_solution(x):
-        return np.exp(10 * (x * x * x) - 13.5 * x * x + 4.2 * x) * 0.1
-    x_data_solution = np.linspace(0, 1, 200)
-    y_data_solution = y_data_solution(x_data_solution)
-    ax.plot(x_data_solution, y_data_solution, label=r'$y(x)=0.1 \cdot e^{10x^3-13.5x^2+4.2x}$')
+def add_solution_plot_to_axes(ax):
+    x = np.linspace(0, 1, 200)
+    y = np.exp(10 * (x * x * x) - 13.5 * x * x + 4.2 * x) * 0.1
+    ax.plot(x, y, label=r'$y(x)=0.1 \cdot e^{10x^3-13.5x^2+4.2x}$')
 
 
-def add_start_plot_to_axes():
-    global g1, g2, g3
-    x_arguments, y_method_euler, y_method_cauchy, y_method_trapezium = calculate_x_data_y_data(5)
-    g1, = ax.plot(x_arguments, y_method_euler, label='метод Эйлера')
-    g2, = ax.plot(x_arguments, y_method_cauchy, label='метод Коши')
-    g3, = ax.plot(x_arguments, y_method_trapezium, label='метод трапеций')
+def add_start_plot_to_axes(ax):
 
+    x_data, y_data_dict = calculate_x_data_y_data(5)
+    method_names = y_data_dict.keys()
+    g = dict(zip(method_names,[None for _ in range(3)]))
+    for method_name in method_names:
+        g[method_name], = ax.plot(x_data, y_data_dict[method_name], label=method_name)
 
-fig, ax = plt.subplots()
-add_solution_plot_to_axes()
-add_start_plot_to_axes()
-ax.grid()
-ax.legend(loc='lower left')
-fig.subplots_adjust(bottom=0.2)
+    return tuple(g.values())
 
 
 def submit(text):
+
     if text.isdigit():
         n = int(text)
         if n > 0:
-            x_arguments, y_method_euler, y_method_cauchy, y_method_trapezium = calculate_x_data_y_data(n)
-            g1.set_xdata(x_arguments)
-            g1.set_ydata(y_method_euler)
-            g2.set_xdata(x_arguments)
-            g2.set_ydata(y_method_cauchy)
-            g3.set_xdata(x_arguments)
-            g3.set_ydata(y_method_trapezium)
-            plt.draw()
-
-
-axes_box = fig.add_axes([0.125, 0.05, 0.3, 0.075]) #plt.axes([0.125, 0.05, 0.775, 0.075])
-initial_text = '5'
-text_box = TextBox(axes_box, 'N', initial=initial_text)
-text_box.on_submit(submit)
-
-check_axes = fig.add_axes([0.525, 0.05, 0.1, 0.075])
-check_box = CheckButtons(check_axes, ['Сетка'], [True])
-check_grid = True
+            x_data, y_data_dict = calculate_x_data_y_data(n)
+            y_data = list(y_data_dict.values())
+            g = [g1, g2, g3]
+            for i in range(3):
+                g[i].set_xdata(x_data)
+                g[i].set_ydata(y_data[i])
+            fig.canvas.draw_idle()
 
 
 def on_check_clicked(label):
     global check_grid
+
     if label == 'Сетка':
         check_grid = not check_grid
     ax.grid(check_grid)
-    plt.draw()
+    fig.canvas.draw_idle()
 
 
+fig, ax = plt.subplots()
+fig.canvas.set_window_title('Лабораторная работа №5')
+add_solution_plot_to_axes(ax)
+g1, g2, g3 = add_start_plot_to_axes(ax)
+ax.legend(loc='lower left')
+fig.subplots_adjust(bottom=0.2)
+
+box_axes = fig.add_axes([0.125, 0.05, 0.3, 0.075])
+text_box = TextBox(box_axes, 'N', initial='5')
+text_box.on_submit(submit)
+
+check_axes = fig.add_axes([0.525, 0.05, 0.1, 0.075])
+check_grid = False
+check_box = CheckButtons(check_axes, ['Сетка'], [check_grid])
 check_box.on_clicked(on_check_clicked)
-plt.gcf().canvas.set_window_title('Лабораторная работа №5')
+
 plt.show()
--- method.py
+++ method.py
@@ -3,6 +3,7 @@ class MethodBase:
     def __init__(self, n, f):
         self.__h = 1 / n
         self.__f = f
+        self.dict_methods = self.__get_dict_methods()
 
     @property
     def h(self):
@@ -14,15 +15,25 @@ class MethodBase:
     def method_cauchy(self, x, y):
         return y + self.h * self.__f(x + self.h / 2, y + (self.h / 2) * self.__f(x, y))
 
+    def __get_dict_methods(self):
+        method_names = ['метод Эйлера', 'метод Коши']
+        methods = [self.method_euler, self.method_cauchy]
+        return dict(zip(method_names, methods))
+
 
 class Method(MethodBase):
 
     def __init__(self, n):
         super().__init__(n, Method.__f)
+        self.__get_methods()
 
     def method_trapezium(self, x, y):
         return (2 * y + self.h * self.__f(x, y)) / (2 - self.h * self.__f(x + self.h, 1))
 
+    def __get_methods(self):
+        self.dict_methods['метод трапеций'] = self.method_trapezium
+
     @staticmethod
     def __f(x, y):
         return 30 * y * (x - 0.2) * (x - 0.7)
+

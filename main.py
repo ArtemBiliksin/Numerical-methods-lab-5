f92686161935ef31431d683984912bd53b46ac0e
ArtemBiliksin <ArtemBiliksin@users.noreply.github.com>
2021-04-29 14:12:16 +0500

    commit

--- main.py
+++ main.py
@@ -1,82 +1,67 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 from matplotlib.widgets import TextBox, CheckButtons
 from method import Method, SolutionCauchyProblem
 
 
-def calculate_x_data_y_data(n):
-    method = Method(n)
-    x_data = np.linspace(0, 1, n)
-    dict_methods = method.dict_methods
-    y_data_dict = dict(zip(dict_methods.keys(), [[0.1] for _ in range(3)]))
-    for x in x_data[1:]:
-        for method_name in dict_methods:
-            y_data_dict[method_name].append(dict_methods[method_name](x, y_data_dict[method_name][-1]))
-    return x_data, y_data_dict
+def add_plot(name_class_method, plot, n=5):
+    method = name_class_method(n)
+    plot.set_xdata(method.x_data)
+    plot.set_ydata(method.y_data)
+    fig.canvas.draw_idle()
 
 
-def print_table_values(x_data, y_data_dict):
-    solution = SolutionCauchyProblem(x_data)
-    y_data_dict[solution.name()] = solution.y_data
-    df = pd.DataFrame(y_data_dict, index=x_data)
-    df.index.name = 'x'
-    print(df)
-    print('')
-
-
-def add_solution_plot_to_axes(ax):
-    solution = SolutionCauchyProblem(200)
-    ax.plot(solution.x_data, solution.y_data, label=solution.name_tex_notation())
-
-
-def add_start_plot_to_axes(ax):
-    x_data, y_data_dict = calculate_x_data_y_data(5)
-    method_names = y_data_dict.keys()
-    g = dict(zip(method_names, [None for _ in range(len(method_names))]))
-    for method_name in method_names:
-        g[method_name], = ax.plot(x_data, y_data_dict[method_name], label=method_name)
-    print_table_values(x_data, y_data_dict)
-    return tuple(g.values())
+def add_plots(name_class_method, plot_dict, n=5):
+    method = name_class_method(n)
+    x_data = method.x_data
+    y_data_dict = method.y_data_dict
+    for plot in plot_dict:
+        plot_dict[plot].set_xdata(x_data)
+        plot_dict[plot].set_ydata(y_data_dict[plot])
+    fig.canvas.draw_idle()
 
 
 def submit(text):
     if text.isdigit():
         n = int(text)
         if n > 0:
-            x_data, y_data_dict = calculate_x_data_y_data(n)
-            y_data = list(y_data_dict.values())
-            print_table_values(x_data, y_data_dict)
-            g = [ax1, ax2, ax3]
-            for i in range(3):
-                g[i].set_xdata(x_data)
-                g[i].set_ydata(y_data[i])
-            fig.canvas.draw_idle()
+            pass
+            add_plots(Method, plot_dict, n)
 
 
 def on_check_clicked(label):
-    global check_grid
+    global visible_grid
     if label == 'Сетка':
-        check_grid = not check_grid
-    ax.grid(check_grid)
+        visible_grid = not visible_grid
+    ax.grid(visible_grid)
     fig.canvas.draw_idle()
 
 
-fig, ax = plt.subplots()
-fig.canvas.set_window_title('Лабораторная работа №5')
-add_solution_plot_to_axes(ax)
-ax1, ax2, ax3 = add_start_plot_to_axes(ax)
-ax.legend(loc='lower left')
-fig.subplots_adjust(bottom=0.2)
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    fig.canvas.set_window_title('Лабораторная работа №5')
+    ax.set(xlim=(-0.1, 1.1), ylim=(0, 0.3), xticks=np.arange(0, 1.1, 0.1), yticks=np.arange(0, 0.31, 0.025))
+    ax_solution, = ax.plot([], [], label=SolutionCauchyProblem.name_tex_notation())
+    add_plot(SolutionCauchyProblem, ax_solution, 200)
 
-box_axes = fig.add_axes([0.125, 0.05, 0.3, 0.075])
-text_box = TextBox(box_axes, 'N', initial='5')
-text_box.on_submit(submit)
+    plot_dict = dict(zip(Method.get_name_methods(), [None for _ in range(len(Method.get_name_methods()))]))
+    for plot in plot_dict:
+        plot_dict[plot], = ax.plot([], [], label=plot)
+    add_plots(Method, plot_dict)
 
-check_axes = fig.add_axes([0.525, 0.05, 0.1, 0.075])
-check_grid = False
-check_box = CheckButtons(check_axes, ['Сетка'], [check_grid])
-check_box.on_clicked(on_check_clicked)
+    ax.legend(loc='upper left')
 
-plt.show()
+    fig.subplots_adjust(bottom=0.2)
+
+    box_axes = fig.add_axes([0.125, 0.05, 0.3, 0.075])
+    text_box = TextBox(box_axes, 'N', initial='5')
+    text_box.on_submit(submit)
+
+    visible_grid = True
+    ax.grid(linestyle='--', visible=visible_grid)
+    check_axes = fig.add_axes([0.525, 0.05, 0.1, 0.075])
+    check_box = CheckButtons(check_axes, ['Сетка'], [visible_grid])
+    check_box.on_clicked(on_check_clicked)
+
+    plt.show()
--- method.py
+++ method.py
@@ -1,48 +1,51 @@
 import numpy as np
 
 
-class MethodBase:
-    def __init__(self, n, f):
-        self.__h = 1 / n
-        self.__f = f
-        self.dict_methods = self.__get_dict_methods()
-
-    @property
-    def h(self):
-        return self.__h
-
-    def method_euler(self, x, y):
-        return y + self.h * self.__f(x, y)
-
-    def method_cauchy(self, x, y):
-        return y + self.h * self.__f(x + self.h / 2, y + (self.h / 2) * self.__f(x, y))
-
-    def __get_dict_methods(self):
-        return {'метод Эйлера': self.method_euler, 'метод Коши': self.method_cauchy}
-
-
-class Method(MethodBase):
+class Method:
     def __init__(self, n):
-        super().__init__(n, Method.__f)
-        self.__get_methods()
+        self.__h = 1 / n
+        self.__f = lambda x, y: 30 * y * (x - 0.2) * (x - 0.7)
+        self.dict_methods = {'метод Эйлера': self.__method_euler,
+                             'метод Коши': self.__method_cauchy,
+                             'метод трапеций': self.__method_trapezium}
+        self.dict = Method.get_name_methods()
+        self.x_data = np.linspace(0, 1, n)
+        self.y_data_dict = self.__calculate_y_data_dict()
 
-    def method_trapezium(self, x, y):
-        return (2 * y + self.h * self.__f(x, y)) / (2 - self.h * self.__f(x + self.h, 1))
+    def __method_euler(self, x, y):
+        return y + self.__h * self.__f(x, y)
 
-    def __get_methods(self):
-        self.dict_methods['метод трапеций'] = self.method_trapezium
+    def __method_cauchy(self, x, y):
+        return y + self.__h * self.__f(x + self.__h / 2, y + (self.__h / 2) * self.__f(x, y))
+
+    def __method_trapezium(self, x, y):
+        return (2 * y + self.__h * self.__f(x, y)) / (2 - self.__h * self.__f(x + self.__h, 1))
+
+    def __method_runge(self, x, y):
+        h = self.__h
+        f = self.__f
+        k1 = h * f(x, y)
+        k2 = h * f(x + h / 2, y + h / 2 * k1)
+        k3 = h * f(x + h / 2, y + h / 2 * k2)
+        k4 = h * f(x + h, y + h * k3)
+        return y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
     @staticmethod
-    def __f(x, y):
-        return 30 * y * (x - 0.2) * (x - 0.7)
+    def get_name_methods():
+        return ['метод Эйлера', 'метод Коши', 'метод трапеций']
+
+    def __calculate_y_data_dict(self):
+        y_data_dict = dict(zip(self.dict_methods.keys(), [[0.1] for _ in range(len(self.dict_methods))]))
+        for x in self.x_data[1:]:
+            for method_name in self.dict_methods:
+                y = self.dict_methods[method_name](x, y_data_dict[method_name][-1])
+                y_data_dict[method_name].append(y)
+        return y_data_dict
 
 
 class SolutionCauchyProblem:
-    def __init__(self, arg):
-        if isinstance(arg, int):
-            self.x_data = np.linspace(0, 1, arg)
-        else:
-            self.x_data = arg
+    def __init__(self, n):
+        self.x_data = np.linspace(0, 1, n)
         self.y_data = 0.1 * np.exp(self.x_data * (10 * (self.x_data * self.x_data) - 13.5 * self.x_data + 4.2))
 
     @staticmethod
